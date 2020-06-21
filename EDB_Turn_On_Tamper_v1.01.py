c3865e2cf8c902782cd314936a91ba9f1e47f215
Sophos Professional Services API Scripts <64469914+sophosukps@users.noreply.github.com>
2020-06-21 18:49:16 +0100

    Update and rename EDB_Turn_On_Tamper_v1.0.py to EDB_Turn_On_Tamper_v1.01.py

--- EDB_Turn_On_Tamper_v1.01.py
+++ EDB_Turn_On_Tamper_v1.01.py
@@ -1,6 +1,6 @@
 # These scripts are examples and unsupported
-#Make sure requests is installed
-#Turn on EDB Tamper v1.0
+# Make sure requests is installed
+# Turn on EDB Tamper v1.01
 import requests
 import csv
 import configparser
@@ -8,7 +8,7 @@ import json
 # Import datetime modules
 from datetime import date
 from datetime import datetime
-#Import OS to allow to check which OS the script is being run on
+# Import OS to allow to check which OS the script is being run on
 import os
 today = date.today()
 now = datetime.now()
@@ -57,46 +57,46 @@ def get_whoami():
     return organizationID, organization_Header, check_organization_type
 
 def get_all_sub_estates(organization):
-    #Add X-Organization-ID to the headers dictionary
+    # Add X-Organization-ID to the headers dictionary
     headers[organizationHeader] = organization
-    #URL to get the list of tennants
+    # URL to get the list of tennants
     sub_estate_url = 'https://api.central.sophos.com/' + organizationType + '/v1/tenants?pageTotal=true'
-    #Request all tenants
+    # Request all tenants
     request_sub_estates = requests.get(sub_estate_url, headers=headers)
-    #Convert to JSON
+    # Convert to JSON
     sub_estate_json = request_sub_estates.json()
-    #Set the keys you want in the list
-    #Find the number of pages we will need to search to get all the sub estates
+    # Set the keys you want in the list
+    # Find the number of pages we will need to search to get all the sub estates
     total_pages = sub_estate_json["pages"]["total"]
-    #Paged URL https://api.central.sophos.com/organization/v1/tenants?page=2 add total pages in a loop
+    # Paged URL https://api.central.sophos.com/organization/v1/tenants?page=2 add total pages in a loop
     sub_estate_keys = ('id', 'name', 'dataRegion')
-    #Add the tenants to the sub estate list
+    # Add the tenants to the sub estate list
     for all_sub_estates in sub_estate_json["items"]:
         #Make a temporary Dictionary to be added to the sub estate list
         sub_estate_dictionary = {key:value for key, value in all_sub_estates.items() if key in sub_estate_keys}
         sub_estate_list.append(sub_estate_dictionary)
-    #Remove X-Organization-ID from headers dictionary. We don't need this anymore
+    # Remove X-Organization-ID from headers dictionary. We don't need this anymore
     del headers[organizationHeader]
 
 def get_all_computers(sub_estate_token, url, name):
-    #Get all Computers from sub estates
+    # Get all Computers from sub estates
     computers_url = url
-    #Loop while the page_count is not equal to 0. We have more computers to query
+    # Loop while the page_count is not equal to 0. We have more computers to query
     page_count = 1
     while page_count != 0:
-        #Sub estate to be searched
+        # Sub estate to be searched
         sub_estate_id = sub_estate_token
-        #Add X-Tenant-ID to the headers dictionary
+        # Add X-Tenant-ID to the headers dictionary
         headers['X-Tenant-ID'] = sub_estate_id
-        #Add X-Tenant-ID to the post_headers dictionary
+        # Add X-Tenant-ID to the post_headers dictionary
         post_headers['X-Tenant-ID'] = sub_estate_id
-        #Request all Computers
+        # Request all Computers
         request_computers = requests.get(computers_url, headers=headers)
         if request_computers.status_code != 200:
             return
-        #Convert to JSON
+        # Convert to JSON
         computers_json = request_computers.json()
-        #Set the keys you want in the list
+        # Set the keys you want in the list
         computer_keys = ('id', 'hostname', 'lastSeenAt', 'tamperProtectionEnabled', 'Sub Estate', 'type')        #Add the computers to the computers list
         for all_computers in computers_json["items"]:
             # Make a temporary Dictionary to be added to the sub estate list
@@ -110,17 +110,17 @@ def get_all_computers(sub_estate_token, url, name):
                     computer_dictionary['Machine_URL'] = 'N/A'
                     computer_dictionary['Sub Estate'] = name
                     computer_list.append(computer_dictionary)
-                    #Turn on Tamper and return status code. 201 successful
+                    # Turn on Tamper and return status code. 201 successful
                     result_code = turn_on_tamper(computer_dictionary['id'], url, post_headers)
                     if result_code.status_code == 201:
                         computer_dictionary['tamperProtectionEnabled'] = 'Successful'
                     else:
                         computer_dictionary['tamperProtectionEnabled'] = 'Failed'
-            #This line allows you to debug on a certain computer. Add computer name
+            # This line allows you to debug on a certain computer. Add computer name
             if 'mc-nuc-splunk' == computer_dictionary['hostname']:
                 print('Add breakpoint here', computer_dictionary['hostname'])
-        #Check to see if you have more than 50 machines by checking if nextKey exists
-        #We need to check if we need to page through lots of computers
+        # Check to see if you have more than 50 machines by checking if nextKey exists
+        # We need to check if we need to page through lots of computers
         if 'nextKey' in computers_json['pages']:
             next_page = computers_json['pages']['nextKey']
             # Change URL to get the next page of computers
@@ -131,7 +131,6 @@ def get_all_computers(sub_estate_token, url, name):
             page_count = 0
 
 def turn_on_tamper(machineID,endpoint_url,post_header):
-    #print(machineID, endpoint_url)
     full_endpoint_url = f"{endpoint_url}{'/'}{machineID}{'/'}{'tamper-protection'}"
     tamper_status = {'enabled': 'true'}
     result = requests.post(full_endpoint_url,data=json.dumps(tamper_status), headers=post_header)
@@ -147,20 +146,10 @@ def get_days_since_last_seen(report_date):
     days = (today - convert_last_seen_to_a_date).days
     return days
 
-def get_threats(endpoint_url, endpoint_id):
-    full_enpoint_url = endpoint_url + '/' + endpoint_id
-    # https://api-{dataRegion}.central.sophos.com/endpoint/v1/endpoints/id
-    request_threat = requests.get(endpoint_url, headers=headers)
-    # Convert to JSON
-    threat_json = request_threat.json()
-    print('')
-
 def make_valid_client_id(os, machine_id):
-    # Characters to be removed
-    # https://central.sophos.com/manage/server/devices/servers/b10cc611-7805-7419-e9f0-46947a4ab60e/summary
-    # https://central.sophos.com/manage/endpoint/devices/computers/60b19085-7bbf-44ff-3a67-e58a3c4e14b1/summary
     Server_URL = 'https://central.sophos.com/manage/server/devices/servers/'
     Endpoint_URL = 'https://central.sophos.com/manage/endpoint/devices/computers/'
+    # Characters to be removed
     # Remove the - from the id
     remove_characters_from_id = ['-']
     for remove_each_character in remove_characters_from_id:
